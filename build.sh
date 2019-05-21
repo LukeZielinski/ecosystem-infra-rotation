a1f59caafed974079e183120e890a2c9eb422ea5
Philip Jägenstedt <philip@foolip.org>
2019-05-21 23:43:58 +0200

    Deploy from master using GitHub Actions (#9)
    Based on https://github.com/foolip/mdn-bcd-collector/blob/47a8640/.github/main.workflow

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ set -o pipefail
 # Install webapp dependencies.
 cd webapp
 rm -rf bower_components
-# This will call `bower install`.
-npm install
+# This will call `bower install`. `--unsafe-perm` is needed for GitHub Actions where
+# this script will be run as root.
+npm install --unsafe-perm
